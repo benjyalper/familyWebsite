097f993d1fdb8fc7ff57ba0149e14510e84699f1
benjyalper <benjyalper@hotmail.com>
2023-08-28 16:04:12 +0300

    added

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 //1. require express (which is a node.js framework used for building the backend- like an electronic screwdriver when node is a regular screwdriver and javascript is a screw) the syntax is: const express = require('express'); but if you add type: 'module'; to the package.json (which is created when we write npm init -y in the console), the we can use the import syntax:
-// import express from 'express';
-const express = require('express');
+import express from 'express';
+
 //same for fs (node native package called file system which we use to write files from input and read them see below)
-const fs = require('fs');
-// import fs from 'fs';
+
+import fs from 'fs';
+
 //and for bodyParser (an npm packege used for parsing json data and we can use it for getting hold of user input sent from the browser to our server)
-const bodyParser = require("body-parser");
-// import bodyParser from 'body-parser';
+// const bodyParser = require("body-parser");
+import bodyParser from 'body-parser';
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 //2.
 const app = express();
@@ -17,9 +21,12 @@ app.use(express.static("public"));
 //4.dont know
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 //6.whith the server running, if we write localhost:3000 in our browser this will send a request to our home route. we handle it like this:
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + "/index.html");//sending the index.html back to the browser
+    res.sendFile(`${__dirname}/index.html`);//sending the index.html back to the browser
 });
 
 //here we are using bodyParser and fs to handle user input from a form
@@ -41,6 +48,6 @@ app.post('/', function (req, res) {
 });
 
 //5. spin up the server by writing in the console: node/nodemon app.js
-app.listen(3000, function () {
+app.listen(port, function () {
     console.log('Server is running on port 3000');
 });
